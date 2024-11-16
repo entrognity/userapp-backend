de56cd2d6f80e4d936c268d66863cc0d753ce5d6
share knowledge <myacc799731@gmail.com>
2024-11-16 18:26:29 +0530

    backend update

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ const cors = require('cors');
 const ordersRoutes = require('./routes/ordersRoutes');
 const filesRoutes = require('./routes/filesRoutes');
 const cartRoutes = require('./routes/cartRoutes');
-const homeRoutes = require('./routes/homeRoutes');
 
 const app = express();
 
 app.use(cors({
-    origin: ['https://userapp-frontend.vercel.app', 'https://www.pinpaper.in', 'https://pinpaper.in' ], // Array for multiple origins
+    origin: ['http://localhost:3000'], // Array for multiple origins
     methods: ['GET', 'POST', 'PUT', 'PATCH'],
-    credentials: true // allows cookies for auth
+    credentials: true
 }));
 
 app.use(morgan('dev'));
@@ -20,7 +19,6 @@ app.use(express.json({ limit: '100kb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/', homeRoutes);
 app.use('/api/v1/orders', ordersRoutes);
 app.use('/api/v1/files', filesRoutes);
 app.use('/api/v1/cart', cartRoutes);
--- routes/homeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeControllers = require('../controllers/homeControllers');
-
-// define routes and their corresponding controllers
-router.get('/', homeControllers.getHome);
-
-module.exports = router;
